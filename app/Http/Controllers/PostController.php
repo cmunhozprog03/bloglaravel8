dec7425df014ced8e82816d5f3e052676b064171
cmunhozprog03h <cmunhozprog03@hotmail.com>
2021-04-23 16:48:09 -0300

    lesson 7 completed

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,10 +1,17 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Post;
 
 use Illuminate\Http\Request;
 
+use function Ramsey\Uuid\v1;
+
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        $posts = Post::where('status', 2)->latest('id')->paginate(8);
+        return view('posts.index', compact('posts'));
+    }
 }
